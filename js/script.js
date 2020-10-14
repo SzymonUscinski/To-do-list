2700afbe947352e052fd2a39ef9a64aa085b2e88
Szymon Uscinski <szymonuscinski@gmail.com>
2020-10-14 20:55:03 +0200

    added immutability to JS code

--- js/script.js
+++ js/script.js
@@ -3,23 +3,29 @@
         console.log("Hello everyone! Nice to see you :)");
     }
 
-    const tasks = [];
+    let tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [...tasks, { content: newTaskContent }];
 
         render();
     };
 
     const removeTask = (index) => {
-        tasks.splice(index, 1);
+        tasks = [...tasks.slice(0, index),
+        ...tasks.slice(index + 1)
+        ];
+
         render();
     };
 
     const toggleTaskDone = (index) => {
-        tasks[index].done = !tasks[index].done;
+        tasks = [
+            ...tasks.slice(0, index),
+            { ...tasks[index], done: !tasks[index].done },
+            ...tasks.slice(index + 1)
+        ];
+
         render();
     };
 
